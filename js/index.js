78d53087b3a0022e27ac36fdfb1ef311661b9290
Malcolm Lynch <milynch1320@gmail.com>
2019-02-22 16:50:56 -0600

    finished overlay for nav system design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+function openNav() {
+    document.querySelector(".dropdown").style.width = "100%";
+    document.querySelector(".menu-close").style.display = "block";
+    document.querySelector(".menu-expand").style.display = "none";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.querySelector(".dropdown").style.width = "0%";
+    document.querySelector(".menu-close").style.display = "none";
+    document.querySelector(".menu-expand").style.display = "block";
+  }
